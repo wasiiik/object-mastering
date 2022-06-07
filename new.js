f8565bae8232793f8302be2c7698994e7297fed4
Wasik Hasan <46458111+wasikhasan@users.noreply.github.com>
2022-06-08 01:34:27 +0600

    new js

--- new.js
+++ new.js
@@ -1,4 +1,20 @@
-class person {
+// class person {
+//     constructor(firstname, lastname, salary) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.salary = salary;
+//     }
+// }
+
+// const heroPerson = new person('Hero', 'Balam', 25000);
+
+// console.log(heroPerson);
+
+// const friendlyPerson = new person('Kalam', 'Ahmed', 30000);
+
+// console.log(friendlyPerson);
+
+class Person {
     constructor(firstname, lastname, salary) {
         this.firstname = firstname;
         this.lastname = lastname;
@@ -6,10 +22,17 @@ class person {
     }
 }
 
-const heroPerson = new person('Hero', 'Balam', 25000);
-
+const heroPerson = new Person('Hero', 'Balam', 2000);
 console.log(heroPerson);
 
-const friendlyPerson = new person('Kalam', 'Ahmed', 30000);
-
+const friendlyPerson = new Person('Hero', 'Kalam', 25000);
 console.log(friendlyPerson);
+
+function Person1(firstname, lastname, salary) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.salary = salary;
+}
+
+const oldPerson = new Person1('Grand', 'Papa', 1200);
+console.log(oldPerson);
